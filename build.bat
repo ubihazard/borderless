05b8c98352a0e3629906295650d35c7fa91405b6
Ubihazard <ubihazard@users.noreply.github.com>
2022-02-15 04:14:34 +0300

    Remove 32-bit flag

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ cd /d "%~dp0"
 rc borderless.rc > nul
 
 :: Build the executable
-clang -O2 -m32 -mwindows -municode %* borderless.c borderless.res -o borderless.exe -luser32 -lgdi32 -lshell32 -lole32 -Wno-deprecated-declarations
+clang -O2 -mwindows -municode %* borderless.c borderless.res -o borderless.exe -luser32 -lgdi32 -lshell32 -lole32 -Wno-deprecated-declarations
 
 :: Embed manifest
 mt -nologo -manifest borderless.exe.manifest -outputresource:"borderless.exe;1"
